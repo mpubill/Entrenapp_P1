81223e104d5507acf977cf63db5b41e00891786e
melan <melan@LAPTOP-30ROFJGU>
2023-06-02 13:55:06 -0600

    Ejercicios superiores (ambos niveles)

---
+++ app/src/main/java/sv/edu/catolica/entrenapp_p1/ej8gym_principianteActivity.java
@@ -0,0 +1,14 @@
+package sv.edu.catolica.entrenapp_p1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ej8gym_principianteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ej8gym_principiante);
+    }
+}
